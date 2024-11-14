e2259f1d1684f03a0fc71b0d4c40dfefe63b1af4
npakk <66075807+npakk@users.noreply.github.com>
2024-11-14 11:59:56 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,4 +2,4 @@
 
 set -e
 umask 022
-exec ruby -e "`curl -fsSL https://raw.githubusercontent.com/z80oolong/linuxbrew-altinstall/master/install`" "$@"
+exec ruby -e "`curl -fsSL https://raw.githubusercontent.com/npakk/linuxbrew-altinstall/master/install`" "$@"
